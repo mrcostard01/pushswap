15cef83c7423e084a8962630d57a80b9b736eff6
mrcostard01 <35123151+mrcostard01@users.noreply.github.com>
2026-01-25 22:46:46 +0100

    fixed problems with  the parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wipion <wipion@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 11:54:51 by wipion            #+#    #+#             */
-/*   Updated: 2026/01/18 21:54:32 by wipion           ###   ########.fr       */
+/*   Updated: 2026/01/25 22:45:17 by wipion           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ int *checkstring(char *str)
             {
                 if ((num[i][j] == '-' && num[i][j + 1] == '-')|| (num[i][j] == '+' && (num[i][j + 1] == '+')))
                     return (NULL);
-                if (ft_isdigit(num[i][j]) == 0 && num[i][j] != '-' && num[i][j] != '+')
+                if (ft_isdigit(num[i][j]) == 0 && num[i][j] != '-' && num[i][j] != '+' && num[i][j] != '/' )
                     return (NULL);
                 j++;
             }
@@ -114,8 +114,9 @@ int *checkstring(char *str)
             j++;
         }
         newtab[j] = 'l';
-        if (checkduplicate(newtab/*, len*/) == -1)
+        if (checkduplicate(newtab) == -1)
             return (NULL);
+        newtab[j] = '\0';
         free(num);
         return (newtab);
 }
@@ -127,7 +128,6 @@ int     main(int ac, char **av)
     t_list  *tmp;
     int    j;
     char    *str;
-    char    *str2;
     t_list  *startnode;
     int len;
     int i;
@@ -139,29 +139,44 @@ int     main(int ac, char **av)
         write(2, "Error\n", 6);
         return (-1);
     }
-    if (av[1] == NULL)
+    if (av[1] == NULL || av[1][0] == '\0')
     {
         write(2, "Error\n", 6);
         return (-1);
     }
-    str = malloc(sizeof(char) * 100);
-    if (!str)
+    while (av[i])
     {
-		free(str);
-		return (-1);
+        len += ft_strlen(av[i]) + 1;
+        i++;
     }
+    len++;
+    
+    i = 1;
+     printf ("%i", len);
     if (ac > 3)
     {
+        len = 0;
         while (av[i])
         {
-            len += ft_strlen(av[i]) + 1;
-            ft_strlcat(str, av[i], len);
-            ft_strlcat(str, " ", len);
+            len += ft_strlen(av[i]);
             i++;
         }
-        str2 = ft_strdup(str);
+        str = malloc(sizeof(char) * (len + i));
+        if (!str)
+        {
+		    free(str);
+		    return (-1);
+        }
+        i = 1;
+        while (av[i])
+        {
+            ft_strlcat(str, av[i], len + i);
+            ft_strlcat(str, " ", len + i);
+            i++;
+        }
+        i = 0;
         reallist = checkstring(str);
-        fakelist = checkstring(str2);
+        fakelist = checkstring(str);
         free(str);
     }
     else
@@ -175,6 +190,7 @@ int     main(int ac, char **av)
             }
             j++;
         }
+
         reallist = checkstring(av[1]);
         fakelist = checkstring(av[1]);
     }
@@ -182,6 +198,7 @@ int     main(int ac, char **av)
     len = 0;
     if (!reallist )
     {
+        free(reallist);
         write(2, "Error\n", 6);
         return (-1);
     }
@@ -189,9 +206,13 @@ int     main(int ac, char **av)
         len++;
     while (reallist[len] != '\0')
         len++;
-    len--;
-    mergesort(reallist,0, len);
-    while (reallist[i] != fakelist[0] )
+    mergesort(reallist,0, len - 1);
+     /*while (i < len)
+    {
+        printf("%i : %i\n", i,reallist[i]);
+        i++;
+    }*/
+    while (reallist[i] != fakelist[0])
         i++;
     startnode = createlist(fakelist[0], i);
     tmp = startnode;
@@ -201,46 +222,79 @@ int     main(int ac, char **av)
         j = 0;
         while (fakelist[i] != reallist[j])
             j++;
+        /*if (j== 2)
+            j--;*/
+        //printf("%i\n",j);
         addafter(startnode,fakelist[i], j);
         startnode=startnode->next;
         i++;
     }
+    i=0;
     j = 0;
-    startnode= tmp;
+    //deleteelement(startnode);
+    startnode = tmp;
+    /*while (i < len)
+    {
+        //printf("%i : %i\n",i,stackb[i]);
+        printf("%i: %i\n", startnode->index, startnode->number);
+        startnode=startnode->next;
+        i++;
+    }*/
+    //printf("%i\n", startnode->next->index);
+    tmp = startnode->next;
     while (j < len)
     {
+            if (startnode->prev->index == j && startnode->index != j)
+            {
+                 write(1, "rra\n",4);
+                 //write(1, "pb\n",3);
+                 //startnode = startnode->next;
+            }
+            /*if (startnode->next->index == j && startnode->index == j)
+            {
+                 write(1, "sa\n",3);
+                 //startnode = startnode->next;
+            }*/
             if (startnode->index == j)
             {
                 write(1, "pb\n",3);
                 //deleteelement(startnode);
+                //startnode = startnode->next;
                 j++;
             }
             else {
                 if (startnode->next->index != j || j == 0)
                     write (1, "ra\n", 3);
-                startnode=startnode->next;
+                startnode = startnode->next;
             }
+            /*if (startnode->index == j)
+            {
+                 write(1, "sa\n",3);
+                 //startnode = startnode->next;
+            }*/
     }
-    i = 0;
+    /*i = 0;
     while (i < len)
     {
         write(1, "rb\n", 3);
         i++;
-    }
+    }*/
     i = 0;
     while (i < len)
     {
         write(1, "pa\n", 3);
         i++;
     }
-    /*i = 0;
+    i = 0;
     startnode = tmp;
-    while (i < len)
+    /*while (i < len)
     {
-        printf("%i : %i\n",i,stackb[i]);
-        //printf("%i : %i\n", startnode->index, startnode->number);
+        //printf("%i : %i\n",i,stackb[i]);
+        printf("%i : %i\n", startnode->index, startnode->number);
         startnode=startnode->next;
         i++;
     }*/
+    //deleteelement(startnode);
     free(reallist);
+    return (0);
 }
